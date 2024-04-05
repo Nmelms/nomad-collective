edf14dc8c766cf3bad9f74e295f8f718f47a4bab
Nick Melms <nick@craftedagency.com>
2024-04-05 10:44:53 -0400

    display profile image and loading

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -1,40 +1,49 @@
 "use client";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const supabase = createClientComponentClient();
 
 const UserProfile = ({ params }) => {
+  const [profileData, setProfileData] = useState(null);
   useEffect(() => {
     const fetchUserData = async () => {
       console.log(params.id);
-
-      const {
-        data: { users },
-        error,
-      } = await supabase.auth.admin.listUsers();
+      const { data, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("display_name", params.id)
+        .single();
 
       if (error) {
         console.error("Error fetching user data:", error);
       } else {
-        console.log("User data:", data, users);
+        setProfileData(data);
       }
     };
 
     fetchUserData();
   }, []);
-
-  // getUserById(params.id)
-  //   .then((user) => {
-  //     console.log(user, "this is the userdata");
-  //     // Render the user profile component with the retrieved user data
-  //     return <div className="user-profile">{params.id}</div>;
-  //   })
-  //   .catch((error) => {
-  //     console.error("Error retrieving user:", error.message);
-  //     // Handle the error and render an appropriate fallback UI
-  //     return <div className="user-profile">Error: {error.message}</div>;
-  //   });
+  if (profileData) {
+    return (
+      <div className="user-profile">
+        <Image
+          width={200}
+          height={200}
+          alt={"profile image"}
+          src={`https://xlvjgjhetfrtaigrimtd.supabase.co/storage/v1/object/public/profile-pictures/${profileData.id}/profile`}
+        />
+        <p>{profileData.display_name}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="user-profile">
+        <p>Loading......</p>
+      </div>
+    );
+  }
 };
 
 export default UserProfile;
